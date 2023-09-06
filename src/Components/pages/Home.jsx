4b53df9681874695da4c1503980f9ffdc80f4527
vishal brijesh kanojiya <vishal.kanojiya@boppotechnologies.com>
2023-09-06 17:40:09 +0530

    updated

--- src/Components/pages/Home.jsx
+++ src/Components/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import '../styles/Home.css'
-import Banner from './pageComponents/banner'
-import IguruWidget from './pageComponents/IguruWidget'
+import Banner from './pageComponents/Banner'
 
 function Home() {
     return (
