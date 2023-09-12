54c2ce20b3795b9deb36b9f8edeb2626886d3978
vishal brijesh kanojiya <vishal.kanojiya@boppotechnologies.com>
2023-09-12 13:26:08 +0530

    changes

--- src/Components/pages/pageComponents/Navbar.jsx
+++ src/Components/pages/pageComponents/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Outlet, Link, NavLink } from "react-router-dom";
 import '../../styles/Home.css'
 import Logo from '../../../assets/main-logo.png'
-import IguruWidget from "./IguruWidget";
+// import IguruWidget from "./IguruWidget";
 import { IoMdArrowDropdown } from 'react-icons/io';
 
 
@@ -23,9 +23,9 @@ function Navbar() {
                     <button class="dropbtn">menu <IoMdArrowDropdown className="Navbar_menu_arrow" /></button>
                     <div class="dropdown-content">
                         <NavLink to='/contact' className='Nav_Links' activeClassName="active">contact</NavLink>
-                        {/* <NavLink to='/contact' className='Nav_Links' activeClassName="active">sign-in</NavLink>
-                        <NavLink to='/contact' className='Nav_Links' activeClassName="active">sign-on</NavLink> */}
-                        <IguruWidget />
+                        <NavLink to='https://staging.insightguru.com/sign-in' className='Nav_Links' activeClassName="active">sign-in</NavLink>
+                        <NavLink to='https://staging.insightguru.com/sign-on/' className='Nav_Links' activeClassName="active">sign-on</NavLink>
+                        {/* <IguruWidget /> */}
                     </div>
                 </div>
             </div>
