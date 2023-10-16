1bba76c54961616c103424e8982c30247a6559cf
vishal brijesh kanojiya <vishal.kanojiya@boppotechnologies.com>
2023-10-16 10:24:49 +0530

    Changes In Navbar According to official site

--- src/Components/pages/pageComponents/Navbar.jsx
+++ src/Components/pages/pageComponents/Navbar.jsx
@@ -1,38 +1,52 @@
 import { Outlet, Link, NavLink } from "react-router-dom";
-import '../../styles/Home.css'
-import Logo from '../../../assets/main-logo.png'
+import "../../styles/Home.css";
+import Logo from "../../../assets/main-logo.png";
 // import IguruWidget from "./IguruWidget";
-import { IoMdArrowDropdown } from 'react-icons/io';
-
-
-
-
+import { IoMdArrowDropdown } from "react-icons/io";
+import { useState } from "react";
 
 function Navbar() {
 
-    return (
-        <>
+    let [Login,setLogin] = useState(false);
 
-            {/* //navbar is here  */}
-            <div className="Navbar">
-                <div className="Nav_logo">
-                    <Link to='/'><img src={Logo} alt="Nav_logo_Image" /></Link>
-                </div>
+  return (
+    <>
+      {/* //navbar is here  */}
+      <div className="Navbar">
+        <div className="Nav_logo">
+          <Link to="/">
+            <img src={Logo} alt="Nav_logo_Image" />
+          </Link>
+        </div>
 
-                <div class="dropdown">
-                    <button class="dropbtn">menu <IoMdArrowDropdown className="Navbar_menu_arrow" /></button>
-                    <div class="dropdown-content">
-                        <NavLink to='/contact' className='Nav_Links' activeClassName="active">contact</NavLink>
-                        <NavLink to='https://staging.insightguru.com/sign-in' className='Nav_Links' activeClassName="active">sign-in</NavLink>
-                        <NavLink to='https://staging.insightguru.com/sign-on/' className='Nav_Links' activeClassName="active">sign-on</NavLink>
-                        {/* <IguruWidget /> */}
-                    </div>
-                </div>
+
+
+        {
+            Login
+            ?
+        <div className="NavProfile">
+          <p className="profile_email">test1@mailinator.com</p>
+          <div className="profile_photo">
+            <i class="fa-solid fa-user"></i>
+          </div>
+        </div>
+          :
+        <div class="dropdown">
+            <button class="dropbtn">menu <IoMdArrowDropdown className="Navbar_menu_arrow" /></button>
+            <div class="dropdown-content">
+                <NavLink to='/contact' className='Nav_Links' activeClassName="active">contact</NavLink>
+                <NavLink to='https://staging.insightguru.com/sign-in' className='Nav_Links' activeClassName="active">sign-in</NavLink>
+                <NavLink to='https://staging.insightguru.com/sign-on/' className='Nav_Links' activeClassName="active">sign-on</NavLink>
             </div>
+        </div>
+        }
 
-            <Outlet />
-        </>
-    )
+
+      </div>
+
+      <Outlet />
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
